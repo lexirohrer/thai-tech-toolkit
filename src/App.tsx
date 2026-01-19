1c8ec16d323ea5a93506ebc1f774d4e3e68fda5b
lexirohrer <53787944+lexirohrer@users.noreply.github.com>
2026-01-18 20:18:00 -0800

    Fix base path for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/thai-tech-toolkit">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/methods/:id" element={<MethodDetail />} />
